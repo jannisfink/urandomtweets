bf925c139d06c3a18fd16be885671c9cab90a014
Jannis Fink <fink@uni-bremen.de>
2017-02-25 03:09:52 +0100

    use config file in same directory as binary

--- config/config.go
+++ config/config.go
@@ -3,14 +3,25 @@ package config
 import (
 	"github.com/BurntSushi/toml"
 	"log"
+	"path/filepath"
+	"os"
+	"path"
 )
 
 const configFile = "config.toml"
 
 var configuration Config
 
+func getConfigFilePath() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return path.Join(dir, configFile)
+}
+
 func LoadConfiguration() {
-	if _, err := toml.DecodeFile(configFile, &configuration); err != nil {
+	if _, err := toml.DecodeFile(getConfigFilePath(), &configuration); err != nil {
 		log.Fatalf("%v", err)
 	}
 }
